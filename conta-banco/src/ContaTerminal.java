986dcdc5a8ea5eea52cd05e0da68acaf9303ef08
Reginaldo <reginaldoengenheiro16@gmail.com>
2024-04-06 00:00:49 -0300

    case de sucesso ;

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //TODO:Conhecer e importar a classe Scanner 
+        
+        //Exibir as mensagens para nosso usuários 
+
+        //Exibir a mensagem conta criada 
+
+        double saldo = 50.00;
+
+        
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá,Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Olá,Digite sua agência: ");
+        int agencia = scanner.nextInt();
+        
+        System.out.println("Olá,Digite sua Conta: ");
+        String conta = scanner.next();
+
+        System.out.printf("Seja bem vido ao nosso banco " + nome, "temos os prazer de atendelo");
+        System.out.printf("Sua agencia é: " + agencia,"temos os prazer de atendelo");
+        System.out.printf("Sua conta é: " + conta,"temos os prazer de atendelo,obrigado por abrir uma conta em nosso");
+
+        
     }
 }
